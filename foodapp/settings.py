454012c2ae982f8f49ce2514d76f3d044caa8c1f
Ian Park <ianpark0412@gmail.com>
2024-09-02 21:35:31 +0900

    fix

--- foodapp/settings.py
+++ foodapp/settings.py
@@ -98,8 +98,8 @@ WSGI_APPLICATION = 'foodapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgresDND',
-        'USER': "postgres",
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("DB_PASSWORD"),
         'HOST': os.environ.get("DB_HOST"),
         'PORT': '5432',
